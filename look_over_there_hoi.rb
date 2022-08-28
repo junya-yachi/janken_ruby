eb64129f9595fb173e4f61211f8a0b46ee898124
junya-yachi <109054387+junya-yachi@users.noreply.github.com>
2022-08-29 03:57:43 +0900

    ruby課題「じゃんけんアプリ」の提出です。

---
+++ look_over_there_hoi.rb
@@ -0,0 +1,62 @@
+class Janken_hoi
+  def rock_paper_scisssor
+    @counter = 0
+    while @counter == 0
+      puts "じゃんけん..."
+      puts "0(グー) 1(チョキ) 2(パー) 3(やめる)"
+      player_hand = gets.to_i
+      enemy_hand = rand(3)
+      
+      puts "ホイ！！"
+      puts "------------"
+      
+      # ジャンケンのコード
+      if player_hand == 3
+        @counter += 1
+        puts "じゃんけんを終了します"
+        break
+      elsif player_hand == 0 || player_hand == 1 || player_hand == 2
+        jankens = ["グー", "チョキ", "パー"]
+        judge = {0 => "あいこで...", 1 => "あなたの負けです。", 2 => "あなたの勝ちです。"}
+        result = (player_hand - enemy_hand + 3) % 3
+        puts "あなた:#{jankens[player_hand]}を出しました。"
+        puts "相手:#{jankens[enemy_hand]}を出しました。"
+        puts judge[result]
+        puts "------------"
+        if result == 2
+          hoi
+        end
+      else
+        puts "その入力は受け付けていません。"
+      end
+    end
+  end
+  
+  def hoi
+    puts "あっち向いて..."
+    puts "1(上) 2(右) 3(下) 4(左)"
+    
+    player_position = gets.to_i - 1
+    enemy_position = rand(4) 
+    puts "ほい！"
+    
+    puts "------------"
+    positions = ["上", "右", "下", "左"]
+    puts "あなた:#{positions[player_position]}を指しました。"
+    puts "相手:#{positions[enemy_position]}を向きました。"
+    
+    if player_position == enemy_position
+      @counter += 1
+      puts "おめでとう！あなたの勝ちです！"
+    else
+      puts "残念！"
+      puts "もう一度..."
+      puts "------------"
+      rock_paper_scisssor
+    end
+  end
+end
+  
+janken_hoi = Janken_hoi.new
+  
+janken_hoi.rock_paper_scisssor()
